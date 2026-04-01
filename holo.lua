96073bd3b5e820fef65c7b96a1e2df12ea01b25b
tetsunem671 <tetsunem67@gmail.com>
2026-04-01 13:30:03 +0800

    Initialize config variable to an empty table
    Ensure config variable is initialized to an empty table if not set.

--- holo.lua
+++ holo.lua
@@ -7,7 +7,7 @@ local pos1 = Vector3.new(203.75982666015625, 398.7754211425781, 138.817993164062
 local pos2 = Vector3.new(-2199.806884765625, 719.1761474609375, 2377.031005859375) -- 🔥 change this
 
 --// STATE
-local config = getenv().CONFIG
+local config = getenv().CONFIG or {}
 local selectedPos = config.Default and pos1 or nil
 local enabled = config.Default and true or false
 
