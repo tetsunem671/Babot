f813c036878d38a082be912978cd45bec50eeb80
tetsunem671 <tetsunem67@gmail.com>
2026-04-01 14:26:23 +0800

    Adjust wait time in anti-spawn-break protection
    Increased wait time for anti-spawn-break protection.

--- holo.lua
+++ holo.lua
@@ -128,7 +128,7 @@ task.spawn(function()
                 hitCount += 1
 
                 -- 🔥 Anti-spawn-break protection
-                task.wait(0.01)
+                task.wait(0.1)
 
                 -- stop overhitting (IMPORTANT)
                 if hitCount > 25 then break end
