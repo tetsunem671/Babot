1830d2e203d856f3601916a0d6ef376874080839
tetsunem671 <tetsunem67@gmail.com>
2026-04-08 22:03:12 +0800

    Update PeeLardenMain.lua

--- PeeLardenMain.lua
+++ PeeLardenMain.lua
@@ -1,12 +1,14 @@
-getgenv().CONFIG = getgenv().CONFIG or {}
-
 -- Shared state
-getgenv().STATE = {}
+getgenv().STATE = loadstring(game:HttpGet("YOUR_UI_URL"))()
+
 
 -- Load modules
 local UI = loadstring(game:HttpGet("YOUR_UI_URL"))()
-local Logic = loadstring(game:HttpGet("YOUR_LOGIC_URL"))()
+local EggBuyer = loadstring(game:HttpGet("PeeLardenBuyer"))()
+
+local Events = loadstring(game:HttpGet("YOUR_EVENT_MODULE_URL"))()
 
 -- Init
+getgenv().STATE.Init()
 UI.Init()
 Logic.Start()
